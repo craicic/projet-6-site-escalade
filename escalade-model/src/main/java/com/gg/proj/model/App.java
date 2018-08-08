cad0ea1807a77fc828dc55bcc1f4ba4c113e3021
xxjokerx <rouzic.jeremie@gmail.com>
2018-08-08 18:27:12 +0200

    Ajout des modules business, consumer, model, technical et webapp

---
+++ escalade-model/src/main/java/com/gg/proj/model/App.java
@@ -0,0 +1,13 @@
+package com.gg.proj.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ escalade-technical/src/main/java/com/gg/proj/technical/App.java
@@ -0,0 +1,13 @@
+package com.gg.proj.technical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
