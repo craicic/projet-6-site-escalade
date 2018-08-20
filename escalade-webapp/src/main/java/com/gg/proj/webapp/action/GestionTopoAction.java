a55871ebc5ab5226d17316fdb6d1f0b3efcc4666
xxjokerx <rouzic.jeremie@gmail.com>
2018-08-20 16:48:17 +0200

    Modification webapp pour affichage liste topos

--- escalade-webapp/src/main/java/com/gg/proj/webapp/action/GestionTopoAction.java
+++ escalade-webapp/src/main/java/com/gg/proj/webapp/action/GestionTopoAction.java
@@ -1,8 +1,25 @@
 package com.gg.proj.webapp.action;
 
+import com.gg.proj.business.impl.manager.TopoManagerImpl;
+import com.gg.proj.model.bean.Topo;
 import com.opensymphony.xwork2.ActionSupport;
+
+import javax.inject.Inject;
+import java.util.List;
 
 public class GestionTopoAction extends ActionSupport {
 
 
+    @Inject
+    private TopoManagerImpl vTopoManagerImpl;
+
+    private List<Topo> listTopo;
+
+
+    public List<Topo> getListTopo() {return listTopo;}
+
+    public String doList(){
+        listTopo = vTopoManagerImpl.list();
+        return ActionSupport.SUCCESS;
+    }
 }
